b9e6483c375528bb6e4294f1de3d134bb0989d24
isajisai <silaspsadia@gmail.com>
2018-07-07 03:22:46 -0700

    support for newline characters in terminal

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -39,6 +39,12 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 }
 
 void terminal_putchar(char c) {
+	// Logical characters shouldn't be rendered, but processed instead
+	if (c == '\n') {
+		terminal_row++;
+		terminal_column = 0; 
+		return;
+	}	
 	unsigned char uc = c;
 	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
