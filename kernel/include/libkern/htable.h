d77d5c793c3b4cdf0f5fe189a8ea9760abd645ec
isajisai <silaspsadia@gmail.com>
2018-08-21 19:06:52 -0400

    htable compatibility with new sllist

--- kernel/include/libkern/htable.h
+++ kernel/include/libkern/htable.h
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "./sllist.h"
+#include <libkern/list.h>
 
-typedef struct htable_t {
+struct htable {
 	int nbuckets;
 	int nitems;
 	int load_factor;
 	unsigned int (*hash_function)(char *, size_t);
-	struct sllnode **buf;
-} htable;
+	struct htable_bucket_entry **buf;
+};
+
+struct htable_bucket_entry {
+	char *item;
+	struct list_head head;
+};
+
+#define HTABLE_BUCKET_ENTRY_INIT(obj) { .item = obj, .head = &(.head) }
 
 htable *htable_init(int, unsigned int (*)(char *, size_t));
-int htable_register(htable *, char *, size_t);
+void htable_register(htable *, char *, size_t);
 unsigned int stdhash(char *, size_t);
 
 htable *htable_init(int nb, unsigned int (*hf)(char *, size_t)) {
@@ -21,26 +28,21 @@ htable *htable_init(int nb, unsigned int (*hf)(char *, size_t)) {
 	ret->hash_function = hf;
 	ret->nitems = 0;
 	ret->load_factor = 0;
-	ret->buf = malloc(nb * sizeof(void *));
+	ret->buf = malloc(nb * sizeof(list_head *));
 	return ret;
 }
 
-int htable_register(htable *ht, char *obj, size_t size) {
-	unsigned int hash = ht->hash_function(obj, size) % ht->nbuckets;
-	struct sllnode *loc = (ht->buf)[hash];
-	struct sllnode *prev = loc;
-	if (loc == NULL) {
-		(ht->buf)[hash] = sllnode_init((int) obj);
-		loc = (ht->buf)[hash];
-		loc->item = (int) obj;
+void htable_register(htable *ht, char *obj, size_t size) {
+	unsigned int index = ht->hash_function(obj, size) % ht->nbuckets;
+	struct htable_bucket_entry *bucket = (ht->buf)[index];
+	if (bucket == NULL) {
+		bucket = HTABLE_BUCKET_ENTRY_INIT(obj);
+		(ht->buf)[index] = bucket;
 	} else {
-		while (loc != NULL) {
-			prev = loc;
-			loc = loc->next;
-		}
-		prev->next = sllnode_init((int) obj);
-	}
-	return 1;
+		struct htable_bucket_entry *new_entry;
+		new_entry = HTABLE_BUCKET_ENTRY_INIT(obj);
+		list_add(new_entry->head, bucket->head);
+	}	
 }
 
 unsigned int stdhash(char *input, size_t size) {
